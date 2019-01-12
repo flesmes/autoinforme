a6bd6346b6895231cc6306f9e8f29434bc4032d9
Felipe Lesmes <flesmesz@aemet.es>
2019-01-12 19:20:34 +0100

    Más comentarios

--- js/animacion.js
+++ js/animacion.js
@@ -5,6 +5,7 @@ var animacion = false;
 var animacionId;
 var imgAnimacion = $('img.animacion');
 
+/* Cambia a las imagenes de índice i */
 function cambiarImg(i) {
     var nombre;
     nombre = (i<10 ? 'RAD0' : 'RAD') + i + 'NAC00_RA2D.PNG';
@@ -13,24 +14,31 @@ function cambiarImg(i) {
     $('#radar-acc').attr('src',nombre);
 }
 
+/* Cambia a la imagen siguiente */
 function siguiente() {
     actual = actual == nImagenes-1 ? 0 : actual+1;
     cambiarImg(actual);
 }
 
+/* Acción para pasar a la imagen siguiente */
+/* Solo si no hay animación en marcha */
 function siguienteListener() {
     if (! animacion) siguiente();
 }
 
+/* Cambia a la imagen anterior */
 function anterior() {
     actual = actual==0 ? nImagenes-1 : actual-1;
     cambiarImg(actual);
 }
 
+/* Acción para pasar a la imagen anterior */
+/* Solo si no hay animación en marcha */
 function anteriorListener() {
     if (! animacion) anterior();
 }
 
+/* Acción de animación */
 function animarListener() {
     if (! animacion) {
 	animacion = true;
@@ -38,12 +46,15 @@ function animarListener() {
     }
 }
 
+/* Acción de parar la animación */
 function pararListener() {
     if (animacion) {
 	animacion = false;
 	clearInterval(animacionId);
     }
 }
+
+/* Acciones asignadas a cada botón */
 
 $('button.prev').click(anteriorListener);
 
--- js/pasada.js
+++ js/pasada.js
@@ -1,23 +1,28 @@
 /* Cambiar pasada */
 
+/* Cambia el valor el estilo "display" para la sección de índice "i" */
 function displaySection(i,style){
     var selector = "#s" + i;
     var section = document.querySelector(selector);
     section.style.display = style;
 }
 
+/* Cambio de pasada. Se esconde la antigua sección activa 
+   y se muestra la nueva */
 function changeActive(){
     displaySection(activo,"none");
     activo = widget.value;
     displaySection(activo,"block");
 }
 
-
+/* Inicialmente la pasada activa es la primera */
+/* Se muestra la pasada activa, las demás se esconden */
 var activo = 1; 
 for (var i = 1; i <= 3; i++) {
      displaySection(i,"none"); 
 } 
 displaySection(activo,"block");
 
+/* Acción para el selector de pasada */
 var widget = document.querySelector("#choose");
 widget.addEventListener("change",changeActive);
